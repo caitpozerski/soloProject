49b8da4a8b39d45f5c59fbf8bbcec3c060945ff6
Caitlyn Pozerski <caitlyn.m.pozerski@gmail.com>
2021-04-21 23:05:08 -0700

    final update

--- dreamulus_project/settings.py
+++ dreamulus_project/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
+
+
+#added for loading secret key from an environment file
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +26,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x7=b9$ywltoe8g)=d5lo!ks$@y5oxo4#-h^s90bwpy@*v#a05*'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+INTERNAL_IPS = ['127.0.0.1']
 
 
 # Application definition
